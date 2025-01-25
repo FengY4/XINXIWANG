bcdbae2f8d1230e9fe2a9c693a171e5345a9ce00
FengYu <15296258498@163.com>
2025-01-25 13:39:52 +0800

    Add files via upload

--- 测试脚本.lua
+++ 测试脚本.lua
@@ -41,7 +41,7 @@ Heartbeat:Connect(HeartbeatUpdate)
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "『禁漫中心』"; Text ="核对用户ID中♧"; Duration = 2; })
 game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "『禁漫中心』"; Text ="用户ID核对完毕♣"; Duration = 4; })
 
-local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/dingding123hhh/hun/main/jmlibrary1.lua"))();        
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/FengY4/XINXIWANG/refs/heads/main/Fwngui.lua"))();        
 local win = ui:new("曹尼玛风御")
 --
 local UITab1 = win:Tab("『信息』",'7734068321')
